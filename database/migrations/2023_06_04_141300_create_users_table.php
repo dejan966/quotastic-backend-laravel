a11f33dc56b2e5189713d4ecd39f074c6586bfc4
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-06-04 16:35:33 +0200

    Created users table

--- database/migrations/2023_06_04_141300_create_users_table.php
+++ database/migrations/2023_06_04_141300_create_users_table.php
@@ -15,6 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('avatar');
+            $table->string('refresh_token');
             $table->timestamps();
         });
     }
