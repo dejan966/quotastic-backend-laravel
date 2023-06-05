9549f8a311d6ea80df3d0439c818088bc8f1e39e
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-06-05 10:43:52 +0200

    Created register validation

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,5 +6,32 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->getCredentials();
+
+        if(!Auth::validate($credentials)):
+            return redirect()->to('login')
+                ->withErrors(trans('auth.failed'));
+        endif;
+
+        $user = Auth::getProvider()->retrieveByCredentials($credentials);
+
+        Auth::login($user);
+
+        return $this->authenticated($request, $user);
+    }
+
+    /**
+     * Handle response after user authenticated
+     * 
+     * @param Request $request
+     * @param Auth $user
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    protected function authenticated(Request $request, $user) 
+    {
+        return redirect()->intended();
+    }
 }
